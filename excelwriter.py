14b442d40cc32db529a8dffb1826202e5d0296ea
Ceyhun Onur <conur@pinc.com>
2019-09-10 14:03:01 +0300

    renamed the main script & reworked the database generator for deeper files (check sample data), improved the script for more dynamic data

--- excelwriter.py
+++ excelwriter.py
@@ -1,3 +1,4 @@
+# -*- coding: utf-8 -*-
 import tempfile
 import sys
 import xlrd
@@ -23,7 +24,7 @@ def generate_style(colour_index, style_code):  # Create style for the cell, main
         pattern.pattern_fore_colour = xlwt.Style.colour_map[title_colours[colour_index]]
     return style
 
-def generate_excel(database):
+def generate_excel(database, filename):
     colour_index = 0  # For generate_style
     workbook = xlwt.Workbook()
     sheet = workbook.add_sheet("main")
@@ -31,19 +32,17 @@ def generate_excel(database):
     tx_count = len(database) - 1  # 1 is for total statistics
     node_count = len(database[1][2])
 
-
     for i in range(tx_count):
         local_statistics_style = generate_style(colour_index, 2)
         title_style = generate_style(colour_index, 1)
         style = generate_style(colour_index, 0)
         colour_index = (colour_index + 1) % 3
-
-        sheet.write_merge(0, 1, 3*i, 3*i+2, "t" + str(i+1) + "\n" + str(database[i+1][0]), title_style)
+        tx_name = database[i+1][0].rsplit('/')[-1]
+        title_name = "t" + str(i+1) + "\n" + tx_name
+        sheet.write_merge(0, 1, 3*i, 3*i+2, title_name, title_style)
         sheet.col(3*i+1).width = 8500  # Set the width of float holding columns so they can fit
         sheet.col(3*i+2).width = 8500
-
         for j in range(node_count):
-
             if j%2 == 0:
                 sheet.write(j+2, 3*i, database[i+1][2][j][0])
                 sheet.write(j+2, 3*i+1, database[i+1][2][j][1])
@@ -61,14 +60,11 @@ def generate_excel(database):
         sheet.write(node_count+4, 3*i, "MAXIMUM", local_statistics_style)
         sheet.write(node_count+4, 3*i+1, "", local_statistics_style)
         sheet.write(node_count+4, 3*i+2, database[i+1][1][2], local_statistics_style)
-
     sheet.write(node_count + 8, 1, "Total Average: ")
     sheet.write(node_count + 9, 1, "Total Minimum: ")
     sheet.write(node_count + 10, 1, "Total Maximum: ")
     sheet.write(node_count + 8, 2, str(database[0][0]))
     sheet.write(node_count + 9, 2, str(database[0][1]))
     sheet.write(node_count + 10, 2, str(database[0][2]))
-
-
-    workbook.save("kshell-data.xls")
-
+    sim_name = filename.split('/')[-2]
+    workbook.save(filename+'/' + sim_name + ".xls")
